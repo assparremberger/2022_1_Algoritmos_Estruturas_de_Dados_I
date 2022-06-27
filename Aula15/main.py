275982094bd2a3cc4d813b62cb06d4cd48f93376
Adalto Selau Sparremberger <adaltoss@me.com>
2022-06-27 19:55:57 -0300

    Aula 15 - Gabarito de Pilhas

---
+++ Aula15/main.py
@@ -0,0 +1,19 @@
+from Pilha import Pilha
+
+livros = Pilha()
+livros.imprimir()
+livros.adicionar( "O tempo e o vento" )
+
+livros.adicionar( "Dom Casmurro" )
+
+livros.adicionar( "O Alquimista" )
+
+livros.remover()
+
+livros.adicionar("Aprendendo sobre Pilhas")
+
+livros.remover()
+livros.remover()
+livros.remover()
+
+livros.adicionar("Aprendendo sobre Filas")
